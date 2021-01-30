36210fafd0cb0dbf28fb25b55415f427d4060dd7
Kolja <55852574+KoljaL@users.noreply.github.com>
2021-01-31 00:00:14 +0100

    Add files via upload

--- functions.js
+++ functions.js
@@ -193,19 +193,342 @@ function item_search() {
 
 
 
+// SHOW TOOLTIP 
+function show_tooltip(ID) {
+	var element = document.getElementById(ID);
+	// console.log('Before');
+	// console.log(element);
+	// console.table(element.style.visibility); 
+	if (element.style.visibility === "hidden" || element.style.visibility === "") {
+		element.style.visibility = "visible";
+		element.style.opacity = 1;
+	} else {
+		element.style.visibility = "hidden";
+		element.style.opacity = 0;
+	}
+	// console.log('After');
+	// console.log(element);
+	// console.table(element.style.visibility); 
+}
 
 
 
-				// var links = item[i].getElementsByTagName("span");
-				// var link = links[0].innerHTML;
-				// var imgs = item[i].getElementsByTagName("img");
-				// var img = imgs[0].outerHTML;
-				// var tooltips = item[i].getElementsByClassName("tooltip");
-				// if (tooltips.length > 0) { var tooltip = tooltips[0].outerHTML; }
-				// var item_edits = item[i].getElementsByClassName("item_edit");
-				// if (item_edits.length > 0) { var item_edit = item_edits[0].outerHTML; }
-				// document.getElementById('search_results').innerHTML += img + ' <span style="color:' + color + '">' + link + tooltip + item_edit + '</span><br>';
-				// document.getElementById('search_results').style.display = 'block';
+// function isTouchDevice() {
+//   return (
+//     ('ontouchstart' in window) 
+//     // (navigator.maxTouchPoints > 0) ||
+//     // (navigator.msMaxTouchPoints > 0)
+//     );
+// }
+// var touch = isTouchDevice();
+// console.log(touch);
+
+if (('ontouchstart' in window) == true){
+  var noTouch = document.getElementById('no_touch_device');
+  noTouch.style.visibility = "visible";
+  console.log('touch');
+} else{
+  console.log('no_touch');
+}
+
+// // CONTEXTMENU
+// function show_context(ID) {
+// 	var notepad = document.getElementById(ID);
+// notepad.addEventListener("contextmenu", function (event) {
+// 	event.preventDefault();
+// 	var ctxMenu = document.getElementById("ctxMenu");
+// 	ctxMenu.style.display = "block";
+// 	ctxMenu.style.left = (event.pageX - 10) + "px";
+// 	ctxMenu.style.top = (event.pageY - 10) + "px";
+// }, false);
+// notepad.addEventListener("click", function (event) {
+// 	var ctxMenu = document.getElementById("ctxMenu");
+// 	ctxMenu.style.display = "";
+// 	ctxMenu.style.left = "";
+// 	ctxMenu.style.top = "";
+// }, false);
+// }
+
+
+// function mouseDown(e, q) {
+// 	e = e || window.event;
+// 	console.log(q);
+
+// 	switch (e.which) {
+// 		case 1: console.log('left'); break;
+// 		case 2: console.log('middle'); break;
+// 		case 3: console.log('right'); break;
+// 	}
+// }
+
+// <a href="#" onmousedown="mouseDown(event,'jj');">aaa</a>
+
+
+
+
+
+
+
+(function() {
+  
+  "use strict";
+  // H E L P E R    F U N C T I O N S
+
+  /**
+   * Function to check if we clicked inside an element with a particular class
+   * name.
+   * 
+   * @param {Object} e The event
+   * @param {String} className The class name to check against
+   * @return {Boolean}
+   */
+  function clickInsideElement( e, className ) {
+    var el = e.srcElement || e.target;
+    
+    if ( el.classList.contains(className) ) {
+      return el;
+    } else {
+      while ( el = el.parentNode ) {
+        if ( el.classList && el.classList.contains(className) ) {
+          return el;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  /**
+   * Get's exact position of event.
+   * 
+   * @param {Object} e The event passed in
+   * @return {Object} Returns the x and y position
+   */
+  function getPosition(e) {
+    var posx = 0;
+    var posy = 0;
+
+    if (!e) var e = window.event;
+    
+    if (e.pageX || e.pageY) {
+      posx = e.pageX;
+      posy = e.pageY;
+    } else if (e.clientX || e.clientY) {
+      posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+      posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+    }
+
+    return {
+      x: posx,
+      y: posy
+    }
+  }
+  // C O R E    F U N C T I O N S  
+  /**
+   * Variables.
+   */
+  var contextMenuClassName = "context-menu";
+  var contextMenuItemClassName = "context-menu__item";
+  var contextMenuLinkClassName = "context-menu__link";
+  var contextMenuActive = "context-menu--active";
+
+  var taskItemClassName = "task";
+  var taskItemInContext;
+	var item_ID;
+
+  var clickCoords;
+  var clickCoordsX;
+  var clickCoordsY;
+
+  var menu = document.querySelector("#context-menu");
+  var menuItems = menu.querySelectorAll(".context-menu__item");
+  var menuState = 0;
+  var menuWidth;
+  var menuHeight;
+  var menuPosition;
+  var menuPositionX;
+  var menuPositionY;
+
+  var windowWidth;
+  var windowHeight;
+
+  /**
+   * Initialise our application's code.
+   */
+  function init() {
+    contextListener();
+    clickListener();
+    keyupListener();
+    resizeListener();
+  }
+
+  /**
+   * Listens for contextmenu events.
+   */
+  function contextListener() {
+    document.addEventListener( "contextmenu", function(e) {
+	  taskItemInContext = clickInsideElement( e, taskItemClassName );
+
+		if (taskItemInContext){
+			item_ID = taskItemInContext.getAttribute("data-id");
+		}
+
+
+    console.table(item_ID);
+    
+
+
+      if (taskItemInContext) {
+        item_ID = taskItemInContext.getAttribute("data-id");
+      }
+
+      var logger = document.getElementById('context-menu');
+      logger.innerHTML = '<label class="tooltip icon" onmousedown="show_tooltip(\'' + item_ID +'\');">&nbsp;  &#128462;</label>&nbsp;';
+      logger.innerHTML += '<form action="" method="post"><button type="submit" name="edit" value="' + item_ID + '" class="icon">✎</button></form>';
+      console.table(item_ID);
+
+
+
+
+       
+
+
+
+      if ( taskItemInContext ) {
+        e.preventDefault();
+        toggleMenuOn();
+        positionMenu(e);
+      } else {
+        taskItemInContext = null;
+        toggleMenuOff();
+      }
+    });
+  }
+
+  /**
+   * Listens for click events.
+   */
+  function clickListener() {
+    document.addEventListener( "click", function(e) {
+      var clickeElIsLink = clickInsideElement( e, contextMenuLinkClassName );
+
+      if ( clickeElIsLink ) {
+        e.preventDefault();
+        menuItemListener( clickeElIsLink );
+      } else {
+        var button = e.which || e.button;
+        if ( button === 1 ) {
+          toggleMenuOff();
+        }
+      }
+    });
+  }
+
+  /**
+   * Listens for keyup events.
+   */
+  function keyupListener() {
+    window.onkeyup = function(e) {
+      if ( e.keyCode === 27 ) {
+        toggleMenuOff();
+      }
+    }
+  }
+
+  /**
+   * Window resize event listener
+   */
+  function resizeListener() {
+    window.onresize = function(e) {
+      toggleMenuOff();
+    };
+  }
+
+  /**
+   * Turns the custom context menu on.
+   */
+  function toggleMenuOn() {
+    if ( menuState !== 1 ) {
+      menuState = 1;
+      menu.classList.add( contextMenuActive );
+    }
+  }
+
+  /**
+   * Turns the custom context menu off.
+   */
+  function toggleMenuOff() {
+    if ( menuState !== 0 ) {
+      menuState = 0;
+      menu.classList.remove( contextMenuActive );
+    }
+  }
+
+  /**
+   * Positions the menu properly.
+   * 
+   * @param {Object} e The event
+   */
+  function positionMenu(e) {
+    clickCoords = getPosition(e);
+    clickCoordsX = clickCoords.x+10;
+    clickCoordsY = clickCoords.y-13;
+	console.table(clickCoords);
+    menuWidth = menu.offsetWidth + 4;
+    menuHeight = menu.offsetHeight + 4;
+
+    windowWidth = window.innerWidth;
+    windowHeight = window.innerHeight;
+
+    if ( (windowWidth - clickCoordsX) < menuWidth ) {
+      menu.style.left = windowWidth - menuWidth + "px";
+    } else {
+      menu.style.left = clickCoordsX + "px";
+    }
+
+    if ( (windowHeight - clickCoordsY) < menuHeight ) {
+      menu.style.top = windowHeight - menuHeight + "px";
+    } else {
+      menu.style.top = clickCoordsY + "px";
+    }
+  }
+
+  /**
+   * Dummy action function that logs an action when a menu item link is clicked
+   * 
+   * @param {HTMLElement} link The link that was clicked
+   */
+  function menuItemListener( link ) {
+    console.log( "Task ID - " + taskItemInContext.getAttribute("data-id") + ", Task action - " + link.getAttribute("data-action"));
+    toggleMenuOff();
+  }
+
+
+
+  /**
+   * Run the app.
+   */
+  init();
+
+})();
+
+
+
+
+ 
+
+
+
+// var links = item[i].getElementsByTagName("span");
+// var link = links[0].innerHTML;
+// var imgs = item[i].getElementsByTagName("img");
+// var img = imgs[0].outerHTML;
+// var tooltips = item[i].getElementsByClassName("tooltip");
+// if (tooltips.length > 0) { var tooltip = tooltips[0].outerHTML; }
+// var item_edits = item[i].getElementsByClassName("item_edit");
+// if (item_edits.length > 0) { var item_edit = item_edits[0].outerHTML; }
+// document.getElementById('search_results').innerHTML += img + ' <span style="color:' + color + '">' + link + tooltip + item_edit + '</span><br>';
+// document.getElementById('search_results').style.display = 'block';
 
 
 
--- index.php
+++ index.php
@@ -405,9 +405,9 @@ $debug_array['$_SESSION'] = $_SESSION;
   
 
 /*‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾ TIDY_TEMPLATE ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾*/
-//ob_start();
+ ob_start();
 require 'template.php';
-//$html = ob_get_clean();
+$html = ob_get_clean();
 $config = array(
     'indent' => true,
     'indent-spaces' => 2,
@@ -418,10 +418,10 @@ $config = array(
     'hide-comments' => true, 
     'wrap' => 0
  );
-//$tidy = new tidy;
-//$tidy->parseString($html, $config, 'utf8');
-//$tidy->cleanRepair();
-//echo $tidy;
+$tidy = new tidy;
+$tidy->parseString($html, $config, 'utf8');
+$tidy->cleanRepair();
+echo $tidy;
  
 /*_________________________________________________ TIDY_TEMPLATE _________________________________________________*/
  
@@ -775,6 +775,13 @@ function grap_favicon($url, $name){
         // $filePath = preg_replace('#\/\/#', '/', $directory . '/' . $domain . '.png');            
         //  Load Favicon
         $content = load($favicon, $DEBUG);
+
+        // If Google API don't know and deliver a default Favicon (World)
+        // if ( isset($random) and $random == 3 and md5($content) == '3ca64f83fdcf25135d87e08af65e68c9' ) {$domain = 'default';  }
+
+        if ($DEBUG == 'debug'){print($content);}
+        if ( (md5($content) == '3ca64f83fdcf25135d87e08af65e68c9') or ($content == '') ) {return;}
+
         // Write 
         $fh = @fopen($filePath, 'wb');
         fwrite($fh, $content);
